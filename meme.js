aa7753e16721cf0f579d5556336b6409a5bd15c7
Leo Abubucker <leokumar@outlook.com>
2023-01-20 08:08:03 -0500

    meme.js addition

---
+++ meme.js
@@ -0,0 +1,6 @@
+// JavaScript source code
+var elements = document.getElementsByClassName("question");
+for (var i = 0, len = elements.length; i < len; i++) {
+    elements[i].classList.remove('antiCheatingEffect');
+}
+console.log("MEMEMEEMEMEMEMEMEMEMEMEEMEMEMMEEME")
