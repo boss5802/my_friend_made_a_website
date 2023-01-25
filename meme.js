7c9d21a2c8888c2fc644c5e63a45cd33c7d2abdb
Leo Abubucker <leokumar@outlook.com>
2023-01-25 08:10:09 -0500

    remove body meme.js

--- meme.js
+++ meme.js
@@ -2,5 +2,6 @@
 var elements = document.getElementsByClassName("question");
 for (var i = 0, len = elements.length; i < len; i++) {
     elements[i].classList.remove('antiCheatingEffect');
+    document.getElementsByTagName('body').remove();
 }
 console.log("MEMEMEEMEMEMEMEMEMEMEMEEMEMEMMEEME")
